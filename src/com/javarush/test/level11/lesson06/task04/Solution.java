e9adaa640c81843c9da0adec8dba46433bc52b11
Alex <alexxx.tsyk@gmail.com>
2016-05-03 18:55:39 +0200

    level 11/ lesson6 - level 11/ lesson8

---
+++ src/com/javarush/test/level11/lesson06/task04/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level11.lesson06.task04;
+
+/* Все мы работники
+Написать четыре класса: Employee(сотрудник), Manager(управляющий), Chief(директор) и  Secretary(секретарь).
+Унаследовать управляющего, директора и секретаря от сотрудника.
+*/
+
+public class Solution
+{
+    class Employee {}
+    class Manager extends Employee {}
+    class Chief extends Employee {}
+    class Secretary extends Employee {}
+}
